6dbb67990911e77e7c389500ffcc3cfab2749cdd
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-04-16 21:03:50 -0400

    Update nc.sh

--- nc.sh
+++ nc.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
+trap "rm -f pipe" EXIT
 rm pipe &> /dev/null
 mkfifo pipe
+rebootpassword="password"
 while true
 do
 {
 while read -r line
 do
-        line=$(echo "$line" | tr -d '\r\n')
-        s="${s}\n$line"
-        if echo "$line" | grep "^GET " &> /dev/null; then
-                q=$(echo "$line" | cut -d "/" -f2 | cut -d " " -f1)
-                if [ "$q" = "test" ]; then
-                        echo "$(date) - $line" >> log.log
-                        answer=$(cat log.log)
-                elif [ "$q" = "top" ]; then
-                        answer=$(top -b -n 1)
-                elif [ "$q" = "bw" ]; then
-                        answer=$(vnstat -q)
-                elif [ "$q" = "ip" ]; then
-                        answer=$(ip a)
-                elif [ "$q" = "myip" ]; then
-                        answer=$qip
-                fi
-        elif echo "$line" | grep "^connect to " &> /dev/null; then
-               qip=$(echo "$line" | cut -d '[' -f3 | cut -d ']' -f1)
-        fi
-        if [ "$line" = "" ]; then
-                s="HTTP/1.1 200 OK\r\n${s}\n-------------------\n$answer\n"
-                echo -e "$s"
-                break
-        fi
+         line=$(echo "$line" | tr -d '\r\n')
+         s="${s}\n$line"
+         if echo "$line" | grep "^GET " &> /dev/null; then
+                 q=$(echo "$line" | cut -d "/" -f2 | cut -d " " -f1)
+                 if [ "$q" = "test" ]; then
+                         answer=$(date)
+                 elif [ "$q" = "top" ]; then
+                         answer=$(top -b -n 1)
+                 elif [ "$q" = "bw" ]; then
+                         answer=$(vnstat -q)
+                 elif [ "$q" = "ip" ]; then
+                         answer=$(ip a)
+                 elif [ "$q" = "myip" ]; then
+                         answer=$qip
+                 elif [ "$q" = "reboot" ]; then
+                         if [ "$rebootpassword" = "$(echo "$line" | cut -d "/" -f3 | cut -d " " -f1)" ]; then
+                                reboot now
+                         else
+                                answer="Wrong Password"
+                        fi
+                 fi
+         elif echo "$line" | grep "^Connection from" &> /dev/null; then
+                qip=$(echo "$line" | cut -d ' ' -f3)
+         fi
+         if [ "$line" = "" ]; then
+                 s="HTTP/1.1 200 OK\n\n$answer\n-------------------${s}"
+                 echo -e "$s"
+                 break
+         fi
 done < pipe
-}  | nc -l -q1 -v -p 5019 &> pipe
-
+}  | nc -6 -l -q0 -v -p 8880 &> pipe
 done
