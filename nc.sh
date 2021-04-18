d4405a90cbd5fe8c0e9325591ff33cc0fe7139fa
Sumiza <64884509+Sumiza@users.noreply.github.com>
2021-04-17 23:52:53 -0400

    Update nc.sh

--- nc.sh
+++ nc.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-trap "rm -f pipe" EXIT
-rm pipe &> /dev/null
-mkfifo pipe
+trap "rm -f pipe6" EXIT
+rm pipe6 &> /dev/null
+mkfifo pipe6
+port=80
 rebootpassword="password"
-while true
-do
-{
-while read -r line
-do
-         line=$(echo "$line" | tr -d '\r\n')
-         s="${s}\n$line"
-         if echo "$line" | grep "^GET " &> /dev/null; then
-                 q=$(echo "$line" | cut -d "/" -f2 | cut -d " " -f1)
-                 if [ "$q" = "test" ]; then
+hostname1=""
+hostname2=""
+hostname3=""
+hostname4=""
+hostname5=""
+
+function host1 {
+
+                 q=$(echo -e "$s" | grep "^GET " | cut -d "/" -f2 | cut -d " " -f1)
+                 if [ "$q" = "time" ]; then
                          answer=$(date)
                  elif [ "$q" = "top" ]; then
                          answer=$(top -b -n 1)
@@ -21,23 +22,56 @@ do
                  elif [ "$q" = "ip" ]; then
                          answer=$(ip a)
                  elif [ "$q" = "myip" ]; then
-                         answer=$qip
+                        answer=$(echo -e "$s" | grep "^Connection from" | cut -d ' ' -f3)
                  elif [ "$q" = "reboot" ]; then
-                         if [ "$rebootpassword" = "$(echo "$line" | cut -d "/" -f3 | cut -d " " -f1)" ]; then
-                                answer="Rebooting now"
-                                reboot now
+                         if [ "$rebootpassword" = "$(echo -e "$s" | grep "^GET " | cut -d "/" -f3 | cut -d " " -f1)" ]; then
+                                /sbin/shutdown -r
+                                answer="Rebooting"
                          else
                                 answer="Wrong Password"
                         fi
                  fi
-         elif echo "$line" | grep "^Connection from" &> /dev/null; then
-                qip=$(echo "$line" | cut -d ' ' -f3)
-         fi
+                s="HTTP/1.1 200 OK\n\n$answer\n-------------------${s}"
+}
+function host2 {
+         exit
+}
+function host3 {
+        exit
+}
+function host4 {
+        exit
+}
+function host5 {
+        exit
+}
+
+while true
+do
+{
+while read -r line
+do
+         line=$(echo "$line" | tr -d '\r\n')
+         s="${s}\n$line"
          if [ "$line" = "" ]; then
-                 s="HTTP/1.1 200 OK\n\n$answer\n-------------------${s}"
-                 echo -e "$s"
-                 break
+                hostname=$(echo -e "$s" | grep "^Host:" | cut -d " " -f2 )
+                if [ "$hostname" = "$hostname1" ]; then
+                       host1
+                elif [ "$hostname" = "$hostname2" ]; then
+                        host2
+                elif [ "$hostname" = "$hostname3" ]; then
+                        host3
+                elif [ "$hostname" = "$hostname4" ]; then
+                        host4
+                elif [ "$hostname" = "$hostname5" ]; then
+                        host5
+                #for testing
+                #else
+                #        s="HTTP/1.1 200 OK\r\n\n------------\n ${s}"
+                fi
+                echo -e "$s"
+                break
          fi
-done < pipe
-}  | nc -6 -l -q0 -v -p 8880 &> pipe
+done < pipe6
+}  | nc -6 -l -N -v -p $port &> pipe6
 done
